0490b0f6cecbb602432e89a714cc087a9b857f3b
thibautmarechal11 <thibautmarechal11@gmail.com>
2025-11-13 18:33:22 +0100

    Début projet javascript snake

---
+++ DEV WEB/SNAKE/script.js
@@ -0,0 +1,9 @@
+var name = "Thibaut";
+var age = 23;
+
+var text1 = "Bonjour, j'ai "
+var text2 = " ans et je m'appelle "
+var text2Lenght = text2.length;
+var textFinal = text1 + age.toString() + text2 + name + ".";
+console.log(textFinal);
+console.log("La longueur de la chaîne de caractères 'text2' est de : " + text2Lenght + " caractères.");
