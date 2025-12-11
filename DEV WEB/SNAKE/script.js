2c77837b39b1144487f9df7345ab5c4e0b5297b9
thibautmarechal11 <thibautmarechal11@gmail.com>
2025-12-11 23:12:08 +0100

    Déplacement du serpent

--- DEV WEB/SNAKE/script.js
+++ DEV WEB/SNAKE/script.js
@@ -1,11 +1,54 @@
-var monTableau = [1, 2, 3, 1, 100];
-var monChiffre = monTableau.length;
+window.onload = function() 
+{
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSize = 30;
+    var canvas, ctx;
+    var delay = 1000;
+    var snakee;
 
-for (var i = 0; i < monTableau.length; i++) {
-    console.log(monTableau[i]);
+    init();
+
+    function init() 
+    {
+
+        canvas = document.createElement("canvas");
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        canvas.style.border = "1px solid black";
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext("2d");
+        snakee = new Sanke([[6,4], [5,4], [4,4]]);
+        refreshCanvas();
+        
+    }
+
+    function refreshCanvas(){
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        snakee.draw();
+        setTimeout(refreshCanvas, delay);
+    }
+
+    function drawBlock(ctx, position)
+    {
+        var x = position[0] * blockSize; 
+        var y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+    function Sanke(body)
+    {
+        this.body = body;
+        this.draw = function()
+        {
+            ctx.save();
+            ctx.fillStyle = "#ff0000";
+            for(var i=0; i < this.body.length; i++)
+            {
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        };
+    }
+
 }
-
-console.log("Mon chiffre est : " + monChiffre);
-
-monTableau.push(200);
-console.log("Après push, mon tableau est : " + monTableau);
